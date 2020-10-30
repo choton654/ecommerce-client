60e0f175aa5545a15c0d29597e4b8edbbcc0c932
Choton <choton654@gmail.com>
2020-10-30 14:05:57 +0530

    ===

--- src/api.js
+++ src/api.js
@@ -1,3 +1,6 @@
-const BASE_URL = "http://localhost:8080";
+const BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://serene-castle-33359.herokuapp.com"
+    : "http://localhost:8080";
 
 export default BASE_URL;
